f56186c4f966a1c64c2cc2ba730f9beb4c9c5af4
Ubuntu <sidokaredev@skripsi-ats.neo.internal>
2025-06-05 21:37:00 +0700

    fix: remove unused handler, route

--- internal/routes/vacancy-routes.go
+++ internal/routes/vacancy-routes.go
@@ -11,7 +11,6 @@ func VacancyRoutes(apiv1 *gin.RouterGroup) {
 	handler := &handlers.VacancyHandler{}
 	apiv1.Use(middlewares.RequestLogs(), middlewares.AuthorizationWithBearer())
 	{
-		// apiv1.Handle("GET", "/vacancies", handler.ReadCacheAsideVacancies)
 		apiv1.Handle("POST", "/vacancies", handler.WriteVacanciesCacheAside)
 		apiv1.Handle("GET", "/vacancies", handler.ReadCacheAsideService)
 		apiv1.Handle("PATCH", "/vacancies", handler.UpdateVacanciesCacheAside)
